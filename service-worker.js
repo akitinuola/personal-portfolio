75f543d8731daa339c2f6904cf1f2f586406daa6
Tinuola Alfred <127518859+akitinuola@users.noreply.github.com>
2024-01-23 20:29:19 +0000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+// sw.js
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('your-app-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/aboutme.html',
+                '/contact.html',
+                '/academichistory.html',
+                '/testimonial.html',
+                '/works.html',
+                '/styles.css',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
